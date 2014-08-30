5caa86526b772ceef54db08c6d70944e7dd13f1e
Christer Wendel <christer.wendel@altrusoft.se>
2014-08-30 21:57:30 +0200

    Switching to puppetlabs_spec_helper

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -3,17 +3,17 @@
 # Configures the autofs master file
 class autofs::configure {
   file { '/etc/auto.master':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/autofs/auto.master',
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/autofs/auto.master',
   }
 
   file { '/etc/auto.master.d':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
   }
 }
